3453a382fbb746522cc9c500c3ded71acc87a5d8
Rasmus Cederdorff <rasmus@cederdorff.com>
2024-02-20 20:56:26 +0100

    Refactor UI styles and form structure

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -44,13 +44,13 @@ export default function Index() {
   }, [fetcher.state]);
 
   return (
-    <div className="p-10 dark:text-gray-200">
+    <div className="p-10 rpi ">
       <h1 className="text-5xl">Work Journal</h1>
-      <p className="mt-2 text-lg text-gray-400 dark:text-gray-500">
+      <p className="mt-2 text-lg text-gray-400">
         Learnings and doings. Updated weekly.
       </p>
 
-      <div className="my-8 border p-3 dark:border-gray-700 rounded-md">
+      <div className="my-8 border px-3 py-4 rounded-md">
         <p className="italic">Create a new entry</p>
 
         <fetcher.Form method="post" className="mt-2">
@@ -58,45 +58,61 @@ export default function Index() {
             className="disabled:opacity-70"
             disabled={fetcher.state === "submitting"}
           >
-            <div className="mb-4">
-              <input
-                type="date"
-                name="date"
-                required
-                className="text-gray-900 dark:bg-gray-700 dark:text-gray-300 py-2 px-3 rounded-md"
-                defaultValue={format(new Date(), "yyyy-MM-dd")}
-              />
-            </div>
-            <div className="mb-4 flex space-x-4">
-              {["work", "learning", "interesting-thing"].map((type, index) => (
-                <label key={type} className="flex items-center space-x-2">
+            <div>
+              <div>
+                <input
+                  type="date"
+                  name="date"
+                  required
+                  className="text-gray-900 px-3 py-2 rounded-md"
+                  defaultValue={format(new Date(), "yyyy-MM-dd")}
+                />
+              </div>
+              <div className="mt-4 space-x-4">
+                <label className="inline-block">
                   <input
                     required
                     type="radio"
-                    className="text-blue-600 dark:bg-gray-800 dark:checked:bg-blue-600"
+                    defaultChecked
+                    className="mr-1"
                     name="type"
-                    value={type}
-                    defaultChecked={index === 0}
+                    value="work"
                   />
-                  <span>
-                    {type
-                      .replace(/-/g, " ")
-                      .replace(/\b\w/g, (l) => l.toUpperCase())}
-                  </span>
+                  Work
                 </label>
-              ))}
+                <label className="inline-block">
+                  <input
+                    type="radio"
+                    className="mr-1"
+                    name="type"
+                    value="learning"
+                  />
+                  Learning
+                </label>
+                <label className="inline-block">
+                  <input
+                    type="radio"
+                    className="mr-1"
+                    name="type"
+                    value="interesting-thing"
+                  />
+                  Interesting thing
+                </label>
+              </div>
             </div>
-            <textarea
-              ref={textareaRef}
-              placeholder="Type your entry..."
-              name="text"
-              className="w-full text-gray-700 dark:bg-gray-700 dark:text-gray-300 py-2 px-3 rounded-md"
-              required
-            />
-            <div className="mt-4 text-right">
+            <div className="mt-4">
+              <textarea
+                ref={textareaRef}
+                placeholder="Type your entry..."
+                name="text"
+                className="w-full text-gray-700 px-3 py-2 rounded-md"
+                required
+              />
+            </div>
+            <div className="mt-2 text-right">
               <button
                 type="submit"
-                className="bg-blue-500 px-6 py-2 font-semibold text-white dark:bg-blue-600 rounded-md"
+                className="bg-blue-500 px-4 py-2 font-semibold text-white rounded-md disabled:opacity-70 disabled:cursor-not-allowed"
               >
                 {fetcher.state === "submitting" ? "Saving..." : "Save"}
               </button>
@@ -105,27 +121,42 @@ export default function Index() {
         </fetcher.Form>
       </div>
 
-      <div className="space-y-12">
+      <div className="mt-12 space-y-12">
         {weeks.map((week) => (
-          <div key={week.dateString} className="mt-12">
-            <p className="font-bold text-lg">
+          <div key={week.dateString}>
+            <p className="font-bold">
               Week of {format(parseISO(week.dateString), "MMMM do")}
             </p>
-            <div className="space-y-4 mt-4">
-              {["work", "learnings", "interestingThings"].map(
-                (category) =>
-                  week[category].length > 0 && (
-                    <div key={category}>
-                      <p className="text-xl capitalize">
-                        {category.replace(/s$/, "").replace(/-/g, " ")}
-                      </p>
-                      <ul className="ml-8 list-disc dark:text-gray-400">
-                        {week[category].map((entry) => (
-                          <li key={entry._id}>{entry.text}</li>
-                        ))}
-                      </ul>
-                    </div>
-                  ),
+            <div className="mt-3 space-y-4">
+              {week.work.length > 0 && (
+                <div>
+                  <p>Work</p>
+                  <ul className="ml-8 list-disc">
+                    {week.work.map((entry) => (
+                      <li key={entry._id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {week.learnings.length > 0 && (
+                <div>
+                  <p>Learning</p>
+                  <ul className="ml-8 list-disc">
+                    {week.learnings.map((entry) => (
+                      <li key={entry._id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {week.interestingThings.length > 0 && (
+                <div>
+                  <p>Interesting things</p>
+                  <ul className="ml-8 list-disc">
+                    {week.interestingThings.map((entry) => (
+                      <li key={entry._id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
               )}
             </div>
           </div>
