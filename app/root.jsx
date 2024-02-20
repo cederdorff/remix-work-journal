b3f7672c71bbc93b411a6f9d6667c7bd6da406ee
Rasmus Cederdorff <rasmus@cederdorff.com>
2024-02-20 14:56:33 +0100

    Add date-fns library and update entry schema

--- app/root.jsx
+++ app/root.jsx
@@ -1,43 +1,31 @@
 import {
   Links,
-  Link,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import styles from "./tailwind.css";
 
-export const links = () => [
+import stylesheet from "~/tailwind.css";
+
+export const links = () => [{ rel: "stylesheet", href: stylesheet }];
+
+export const meta = () => [
   {
-    rel: "stylesheet",
-    href: styles,
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1",
   },
 ];
-
-export function meta() {
-  return [{ title: "Remix + Mongoose" }];
-}
-
 export default function App() {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body className="bg-slate-100 p-4 font-sans text-slate-800">
-        <header className="mb-4 border-b-2 pb-3">
-          <Link to="/" className="text-blue-600 hover:underline">
-            Home
-          </Link>
-          <Link to="/books/new" className="ml-3 text-blue-600 hover:underline">
-            New book
-          </Link>
-        </header>
+      <body>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,34 +1,189 @@
-import { useLoaderData, Link } from "@remix-run/react";
+import { useFetcher, useLoaderData } from "@remix-run/react";
+import { format, parseISO, startOfWeek } from "date-fns";
 import mongoose from "mongoose";
+import { useEffect, useRef } from "react";
 
 export async function loader() {
-  const books = await mongoose.models.Book.find();
-  return books;
+  const entries = await mongoose.models.Entry.find();
+  console.log(entries);
+  return entries;
 }
 
 export default function Index() {
-  const books = useLoaderData();
+  let fetcher = useFetcher();
+  let textareaRef = useRef();
+  let entries = useLoaderData();
+
+  let entriesByWeek = entries.reduce((memo, entry) => {
+    let sunday = startOfWeek(parseISO(entry.date));
+    let sundayString = format(sunday, "yyyy-MM-dd");
+
+    memo[sundayString] ||= [];
+    memo[sundayString].push(entry);
+
+    return memo;
+  }, {});
+
+  let weeks = Object.keys(entriesByWeek)
+    .sort((a, b) => a.localeCompare(b))
+    .map((dateString) => ({
+      dateString,
+      work: entriesByWeek[dateString].filter((entry) => entry.type === "work"),
+      learnings: entriesByWeek[dateString].filter(
+        (entry) => entry.type === "learning",
+      ),
+      interestingThings: entriesByWeek[dateString].filter(
+        (entry) => entry.type === "interesting-thing",
+      ),
+    }));
+
+  useEffect(() => {
+    if (fetcher.state === "idle" && textareaRef.current) {
+      textareaRef.current.value = "";
+      textareaRef.current.focus();
+    }
+  }, [fetcher.state]);
 
   return (
-    <div>
-      <h1 className="mb-4 text-2xl font-bold">Remix + Mongoose</h1>
-      <h2 className="mb-3 text-lg font-bold">
-        Here are a few of my favorite books:
-      </h2>
-      <ul className="ml-5 list-disc">
-        {books.map((book) => {
-          return (
-            <li key={book._id}>
-              <Link
-                to={`/books/${book._id}`}
-                className="text-blue-600 hover:underline"
+    <div className="p-10">
+      <h1 className="text-5xl">Work Journal</h1>
+      <p className="mt-2 text-lg text-gray-400">
+        Learnings and doings. Updated weekly.
+      </p>
+
+      <div className="my-8 border p-3">
+        <p className="italic">Create a new entry</p>
+
+        <fetcher.Form method="post" className="mt-2">
+          <fieldset
+            className="disabled:opacity-70"
+            disabled={fetcher.state === "submitting"}
+          >
+            <div>
+              <div>
+                <input
+                  type="date"
+                  name="date"
+                  required
+                  className="text-gray-900"
+                  defaultValue={format(new Date(), "yyyy-MM-dd")}
+                />
+              </div>
+              <div className="mt-4 space-x-4">
+                <label className="inline-block">
+                  <input
+                    required
+                    type="radio"
+                    defaultChecked
+                    className="mr-1"
+                    name="type"
+                    value="work"
+                  />
+                  Work
+                </label>
+                <label className="inline-block">
+                  <input
+                    type="radio"
+                    className="mr-1"
+                    name="type"
+                    value="learning"
+                  />
+                  Learning
+                </label>
+                <label className="inline-block">
+                  <input
+                    type="radio"
+                    className="mr-1"
+                    name="type"
+                    value="interesting-thing"
+                  />
+                  Interesting thing
+                </label>
+              </div>
+            </div>
+            <div className="mt-4">
+              <textarea
+                ref={textareaRef}
+                placeholder="Type your entry..."
+                name="text"
+                className="w-full text-gray-700"
+                required
+              />
+            </div>
+            <div className="mt-2 text-right">
+              <button
+                type="submit"
+                className="bg-blue-500 px-4 py-1 font-semibold text-white"
               >
-                {book.title}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+                {fetcher.state === "submitting" ? "Saving..." : "Save"}
+              </button>
+            </div>
+          </fieldset>
+        </fetcher.Form>
+      </div>
+
+      <div className="mt-12 space-y-12">
+        {weeks.map((week) => (
+          <div key={week.dateString}>
+            <p className="font-bold">
+              Week of {format(parseISO(week.dateString), "MMMM do")}
+            </p>
+            <div className="mt-3 space-y-4">
+              {week.work.length > 0 && (
+                <div>
+                  <p>Work</p>
+                  <ul className="ml-8 list-disc">
+                    {week.work.map((entry) => (
+                      <li key={entry._id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {week.learnings.length > 0 && (
+                <div>
+                  <p>Learning</p>
+                  <ul className="ml-8 list-disc">
+                    {week.learnings.map((entry) => (
+                      <li key={entry._id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {week.interestingThings.length > 0 && (
+                <div>
+                  <p>Interesting things</p>
+                  <ul className="ml-8 list-disc">
+                    {week.interestingThings.map((entry) => (
+                      <li key={entry._id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
+}
+
+export async function action({ request }) {
+  let formData = await request.formData();
+  let { date, type, text } = Object.fromEntries(formData);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  if (
+    typeof date !== "string" ||
+    typeof type !== "string" ||
+    typeof text !== "string"
+  ) {
+    throw new Error("Bad request");
+  }
+
+  return await mongoose.models.Entry.create({
+    date: new Date(date),
+    type: type,
+    text: text,
+  });
 }
